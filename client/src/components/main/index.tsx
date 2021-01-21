36448caf18eed90a68ab5a428cf9c01753771b0b
Travis Grossen <travis-grossen@pluralsight.com>
2021-01-21 08:23:12 -0700

    Trim things down; update packages

--- client/src/components/main/index.tsx
+++ client/src/components/main/index.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react'
 import styleable, { StyleableProps } from 'react-styleable'
 
-import * as css from './styles.scss'
+import css from './styles.scss'
 
 const Main = (props: StyleableProps) => {
-  const { css } = props
 
-  return <div className={css.main}>Hello, world-dwellers</div>
+  return <div className={props.css.main}>Hello, world-dwellers</div>
 }
 
 export default styleable(css)(Main)
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,20 +1,6 @@
-const webpack = require('webpack')
 const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-
-const plugins = [
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-  }),
-  new webpack.LoaderOptionsPlugin({
-    minimize: true,
-    debug: false,
-  }),
-  new ExtractTextPlugin('[name].css'),
-]
 
 const config = {
-  devtool: 'eval',
   entry: {
     main: ['./src/index.tsx'],
   },
@@ -41,68 +27,10 @@ const config = {
         ],
       },
       {
-        test: /\.css$/,
-        include: [path.resolve(__dirname)],
-        use: ExtractTextPlugin.extract({
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                modules: true,
-                importLoaders: 1,
-                localIdentName: '[local]---[hash:base64:5]',
-              },
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                plugins: (_) => [
-                  require('postcss-import')(),
-                  require('postcss-cssnext')(),
-                ],
-              },
-            },
-          ],
-        }),
-      },
-      {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                modules: true,
-                importLoaders: 1,
-                localIdentName: '[local]---[hash:base64:5]',
-              },
-            },
-            { loader: 'sass-loader' },
-            {
-              loader: 'postcss-loader',
-              options: {
-                plugins: (_) => [
-                  require('postcss-import')(),
-                  require('postcss-cssnext')(),
-                ],
-              },
-            },
-          ],
-        }),
-      },
-      {
-        test: /\.(jpg|png|gif)$/,
         use: [
-          { loader: 'url-loader' },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              progressive: true,
-              optimizationLevel: 7,
-              interlaced: false,
-              pngquant: { quality: '65-90', speed: 4 },
-            },
-          },
+          'style-loader',
+          { loader: 'css-loader', options: { modules: true } },
         ],
       },
     ],
@@ -113,7 +41,6 @@ const config = {
     children: false,
     chunks: false,
   },
-  plugins,
 }
 
 module.exports = config
